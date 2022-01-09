d7f3ffa7c82ba7c65f882138845ce0b0c1ff648f
Ananya Tyagi <ananyatyagi996@gmail.com>
2022-01-10 00:28:17 +0530

    Project Setup

---
+++ src/main/java/com/cntrl/alt/debt/gyandaan/GyanDaanMain.java
@@ -0,0 +1,14 @@
+package com.cntrl.alt.debt.gyandaan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class GyanDaanMain {
+
+
+        public static void main(String[] args){
+            SpringApplication.run(GyanDaanMain.class, args);
+            System.out.print("Executed Main Application");
+        }
+    }
