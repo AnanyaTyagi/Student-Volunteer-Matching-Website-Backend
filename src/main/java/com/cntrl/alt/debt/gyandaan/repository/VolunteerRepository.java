cc081daddc7d6dc5e117fd6d97f95e2d169ea8a0
AnanyaTyagi <ananyatyagi996@gmail.com>
2022-01-16 14:07:28 +0530

    Login functionality

---
+++ src/main/java/com/cntrl/alt/debt/gyandaan/repository/VolunteerRepository.java
@@ -0,0 +1,7 @@
+package com.cntrl.alt.debt.gyandaan.repository;
+
+import com.cntrl.alt.debt.gyandaan.entity.Volunteer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface VolunteerRepository extends CrudRepository<Volunteer, String> {
+}
