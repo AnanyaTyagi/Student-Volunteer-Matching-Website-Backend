21f823095d6fd82b8d77b456a2520396c2bb4364
AnanyaTyagi <ananyatyagi996@gmail.com>
2022-01-18 01:07:08 +0530

    Updated Login and register functionality.

--- src/main/java/com/cntrl/alt/debt/gyandaan/controller/StudentController.java
+++ src/main/java/com/cntrl/alt/debt/gyandaan/controller/StudentController.java
@@ -1,12 +1,15 @@
 package com.cntrl.alt.debt.gyandaan.controller;
 
-import com.cntrl.alt.debt.gyandaan.dto.StudentCreationRequest;
-import com.cntrl.alt.debt.gyandaan.dto.StudentResponse;
-import com.cntrl.alt.debt.gyandaan.dto.StudentUpdateRequest;
+import com.cntrl.alt.debt.gyandaan.dto.*;
 import com.cntrl.alt.debt.gyandaan.entity.Student;
+import com.cntrl.alt.debt.gyandaan.entity.Volunteer;
+import com.cntrl.alt.debt.gyandaan.repository.StudentRepository;
+import com.cntrl.alt.debt.gyandaan.repository.VolunteerRepository;
 import com.cntrl.alt.debt.gyandaan.service.StudentService;
+import org.json.JSONObject;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Optional;
 
 import static com.cntrl.alt.debt.gyandaan.utils.APIConstants.STUDENT_RESOURCE;
 import static com.cntrl.alt.debt.gyandaan.utils.APIConstants.VERSION_1;
@@ -32,21 +36,45 @@ public class StudentController {
     private StudentService studentService;
 
     @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private VolunteerRepository volunteerRepository;
+
+    @Autowired
     private ModelMapper modelMapper;
 
-    @PostMapping(value = "", consumes = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Void> register(@RequestBody StudentCreationRequest studentCreationRequest ) {
-        Student student = modelMapper.map(studentCreationRequest, Student.class);
 
+    HttpHeaders responseHeaders=new HttpHeaders();
+
+
+    @PostMapping(value ="/register", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<RegisterResponse> register(@RequestBody StudentCreationRequest studentCreationRequest ) {
+
+        responseHeaders.set("MyResponseHeader", "MyValue");
+        RegisterResponse registerResponse=new RegisterResponse();
+        Optional<Student> studentCheck = studentRepository.findById(studentCreationRequest.getEmailId());
+        Optional<Volunteer> volunteerCheck= volunteerRepository.findById(studentCreationRequest.getEmailId());
+
+        //checking if student already exists
+        if(!studentCheck.isEmpty()) {
+            registerResponse.setResponse("User already exists");
+            return new ResponseEntity<>(registerResponse, responseHeaders, HttpStatus.BAD_REQUEST);
+        }
+        if(!volunteerCheck.isEmpty()) {
+            registerResponse.setResponse("User already exists with another role");
+            return new ResponseEntity<>(registerResponse, responseHeaders, HttpStatus.BAD_REQUEST);
+        }
+        Student student = modelMapper.map(studentCreationRequest, Student.class);
         student = studentService.registerStudent(student);
 
-        URI location = ServletUriComponentsBuilder
+        /*URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
                 .path("/{username}")
                 .buildAndExpand(student.getEmailId())
-                .toUri();
-        return ResponseEntity.created(location)
-                .build();
+                .toUri();*/
+        registerResponse.setResponse("User Registered Successfully");
+        return new ResponseEntity<>(registerResponse, responseHeaders, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{username}")
@@ -70,5 +98,13 @@ public class StudentController {
                     .build();
         }
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @PostMapping(value="/login", consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<LoginResponse> studentVolunteer(@RequestBody LoginRequest loginRequest) {
+
+      String email=loginRequest.getEmail();
+      String password=loginRequest.getPassword();
+      return studentService.studentLogin(email, password);
     }
 }
---
+++ src/main/java/com/cntrl/alt/debt/gyandaan/dto/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.cntrl.alt.debt.gyandaan.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LoginResponse {
+
+    String firstName;
+    String lastName;
+    String response;
+}
