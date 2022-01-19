85f3fbf926165cfc2e3c4f4586d71aeeaec44da4
AnanyaTyagi <ananyatyagi996@gmail.com>
2022-01-19 12:49:56 +0530

    messaging

--- src/main/java/com/cntrl/alt/debt/gyandaan/service/LoginSignupProfileDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.cntrl.alt.debt.gyandaan.service;
-
-import org.springframework.web.bind.annotation.RequestParam;
-
-public interface LoginSignupProfileDetailsService {
-    String getUsernameLogin(String email,String password, String type);
-    
-}
